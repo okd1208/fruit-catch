3aba9f14dd30b117e69e51d6bc5bd2ce62f283f8
okd <y.okada.upd@gmail.com>
2024-09-07 02:20:37 +0900

    change: rotate animation

--- lib/widgets/fruit_widget.dart
+++ lib/widgets/fruit_widget.dart
@@ -1,23 +1,55 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import '../models/fruit.dart';
 
-class FruitWidget extends StatelessWidget {
+class FruitWidget extends StatefulWidget {
   final Fruit fruit;
   final VoidCallback onTap;
 
   const FruitWidget({Key? key, required this.fruit, required this.onTap}) : super(key: key);
 
   @override
+  _FruitWidgetState createState() => _FruitWidgetState();
+}
+
+class _FruitWidgetState extends State<FruitWidget> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(seconds: 3), // 回転の速度を調整
+      vsync: this,
+    )..repeat(); // 無限に回転
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Positioned(
-      left: fruit.x,
-      top: fruit.y,
+      left: widget.fruit.x,
+      top: widget.fruit.y,
       child: GestureDetector(
-        onTap: onTap,  // タップ時にコールバックを実行
-        child: Image.asset(
-          'assets/${fruit.type}.png',  // 果物の種類に応じた画像を表示
-          width: fruit.size,
-          height: fruit.size,
+        onTap: widget.onTap,
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform.rotate(
+              angle: _controller.value * 1.0 * pi,  // 0から2π（360度）で回転
+              child: child,
+            );
+          },
+          child: Image.asset(
+            'assets/${widget.fruit.type}.png',
+            width: widget.fruit.size,
+            height: widget.fruit.size,
+          ),
         ),
       ),
     );
